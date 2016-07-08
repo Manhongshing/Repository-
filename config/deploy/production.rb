d03bde7a982453e5084a0ace8be60e551217fe4a
showwin <showwin_kmc@yahoo.co.jp>
2016-07-08 09:34:45 +0900

    [fix] rename unicorn.pid

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ set :stage, :production
 set :rails_env, 'production'
 set :deploy_to, '/var/www/fc2play'
 set :config_path, 'config'
-set :unicorn_pid, -> { File.join(shared_path, 'tmp', 'pids', 'unicorn.pid') }
+set :unicorn_pid, -> { File.join(shared_path, 'tmp', 'pids', 'fc2play.pid') }
 set :unicorn_config_path, 'config/unicorn.rb'
 set :unicorn_rack_env, 'production'
 set :unicorn_restart_sleep_time, 3
